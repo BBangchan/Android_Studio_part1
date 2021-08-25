ca608b5548484d549151bb700d83b7aa064b4523
chanjong Bang <ckswhdzn@gwnu.ac.kr>
2021-08-26 03:45:51 +0900

    21.08.26 안드로이드 스튜디오 1일차

---
+++ app/src/main/java/com/example/test29/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.test29;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String str = intent.getExtras().getString("키");
+        EditText editText = findViewById(R.id.edit_text);
+        editText.setText(str);
+        // ((EditText)findViewById(R.id.edit_text)).setText(str); 위의 2줄과 같은 의미
+    }
+}
